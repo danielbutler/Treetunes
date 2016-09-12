32f585e442c657d8f48206c15360a6a8c05002df
Daniel Butler <cactusfly@gmail.com>
2016-09-12 13:40:30 -0400

    setup .js files for initial code

---
+++ app.js
@@ -0,0 +1,7 @@
+var playlist = new PLaylist();
+
+var hereComesTheSun = new Song("Here Comes the Sung", "The Beatles", "2:54");
+var walkingOnSunshine = new Song("Walking on Sunshine", "Katrina and  the Waves", "3:43")
+
+playlist.add(hereComesTheSun);
+playlist.add(walkingOnSunshine);
